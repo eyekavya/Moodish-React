006c3d30b642522fd73fc4efac6276d2c93a2831
eyekavya <kavyasrivastava7830@gmail.com>
2025-01-09 18:01:09 +0530

    add SignUp component and update Tailwind CSS configuration

---
+++ src/components/SignUp/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-lavender-200 to-peach-100">
+        <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-8">
+          <h1 className="text-center text-4xl font-extrabold bg-gradient-to-r from-peach-800 to-lavender-800 text-transparent bg-clip-text tracking-wide mb-8">
+            Moodish
+          </h1>
+
+          {/* Sign-up Form */}
+          <form className="space-y-4">
+          <input
+              type="text"
+              placeholder="Name"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none border-2 focus:border-lavender-600"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none border-2 focus:border-lavender-600"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none border-2 focus:border-lavender-600"
+            />
+            {/* <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition"
+            >
+              Sign Up
+            </button> */}
+            <button
+              type="submit"
+              className="w-full bg-lavender-600 text-white py-3 rounded-lg hover:bg-lavender-800 transition-all duration-200 font-bold disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-lavender-400"
+              disabled={false}
+            >
+              Sign Up
+            </button>
+          </form>
+
+          {/* Login Redirect */}
+          <p className="text-center text-gray-500 mt-4 text-sm">
+            Already have an account?{' '}
+            <a href="/login" className="text-lavender-800 hover:underline">
+              Log in
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SignUp;
